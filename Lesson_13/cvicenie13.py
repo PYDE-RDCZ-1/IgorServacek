ba4ed91b26afb31158a1480c5e5f97882b3697c0
IgorSigelsberg <igy@family.sk>
2024-04-04 12:24:09 +0200

    status 4. aprila 2024

---
+++ Lesson_13/cvicenie13.py
@@ -0,0 +1,51 @@
+# exceptions - výnimky
+"""
+try:
+if x=5:
+    print(">5")
+except
+
+# type error
+result = "Hello " + 42
+tuple (1, 2, 3,)
+tuple [0] = 10
+
+# name errror
+
+print(bata)
+
+# value error
+number = int("abc!")
+"""
+# zero division error
+try:
+    a = 10 / 0
+except ZeroDivisionError:
+    print("chyba: delenie nulou")
+else:
+    print("nevyskytla sa chyba")
+finally:
+# tento blok sa obvykle používa k čisteniu
+    print("Tento blok sa vykoná vždy")
+
+
+# cvicenie, program pre zadanie veku, ak nie je platne cislo, spracuje vynimku a vyzve uzivatela, aby zadal
+def vek():
+    cyklus = True
+    while cyklus:
+        try:
+            a = int(input("zadaj Tvoj vek, celé, Číslo:"))
+        except ValueError:
+            print("zadali ste chybné číslo")
+        else:
+            print("zadali ste správne číslo: ", a)
+            cyklus = False
+    return a
+
+
+def main():
+    b = vek()
+    print(vek)
+aasd
+
+
